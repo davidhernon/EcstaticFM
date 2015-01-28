e2b4caa147fc35ffe9041aaecad7e0c5880d1bdb
davidhernon <david.hernon@mail.mcgill.ca>
2015-01-28 17:40:42 -0500

    Initial Commit

---
+++ EcstaticFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EcstaticFM
+//
+//  Created by David Hernon on 2015-01-28.
+//  Copyright (c) 2015 David Hernon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EcstaticFM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EcstaticFM
+//
+//  Created by David Hernon on 2015-01-28.
+//  Copyright (c) 2015 David Hernon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
