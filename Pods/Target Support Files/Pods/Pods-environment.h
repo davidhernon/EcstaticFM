742375bc7805fb4dc848c5afa19e5d252696b95d
davidhernon <david.hernon@mail.mcgill.ca>
2015-02-17 16:57:03 -0500

    Added Podfiles for SoundCloud Integration

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaSoundCloudAPI
+#define COCOAPODS_POD_AVAILABLE_CocoaSoundCloudAPI
+#define COCOAPODS_VERSION_MAJOR_CocoaSoundCloudAPI 1
+#define COCOAPODS_VERSION_MINOR_CocoaSoundCloudAPI 0
+#define COCOAPODS_VERSION_PATCH_CocoaSoundCloudAPI 1
+
+// CocoaSoundCloudUI
+#define COCOAPODS_POD_AVAILABLE_CocoaSoundCloudUI
+#define COCOAPODS_VERSION_MAJOR_CocoaSoundCloudUI 1
+#define COCOAPODS_VERSION_MINOR_CocoaSoundCloudUI 0
+#define COCOAPODS_VERSION_PATCH_CocoaSoundCloudUI 5
+
+// JSONKit
+#define COCOAPODS_POD_AVAILABLE_JSONKit
+#define COCOAPODS_VERSION_MAJOR_JSONKit 1
+#define COCOAPODS_VERSION_MINOR_JSONKit 4
+#define COCOAPODS_VERSION_PATCH_JSONKit 0
+
+// NXOAuth2Client
+#define COCOAPODS_POD_AVAILABLE_NXOAuth2Client
+#define COCOAPODS_VERSION_MAJOR_NXOAuth2Client 1
+#define COCOAPODS_VERSION_MINOR_NXOAuth2Client 2
+#define COCOAPODS_VERSION_PATCH_NXOAuth2Client 1
+
+// OHAttributedLabel
+#define COCOAPODS_POD_AVAILABLE_OHAttributedLabel
+#define COCOAPODS_VERSION_MAJOR_OHAttributedLabel 3
+#define COCOAPODS_VERSION_MINOR_OHAttributedLabel 5
+#define COCOAPODS_VERSION_PATCH_OHAttributedLabel 5
+
