e8d113f34dfe830acc9685aa733075254758e9b6
Kizma <jonathandupre@Jonathans-MacBook-Pro.local>
2015-05-08 19:02:44 -0400

    switched to white stream style & spread gradient layer over the two sound room screens.

---
+++ PopularHeader.h
@@ -0,0 +1,13 @@
+//
+//  PopularHeader.h
+//  EcstaticFM
+//
+//  Created by Kizma on 2015-05-08.
+//  Copyright (c) 2015 David Hernon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopularHeader : UITableViewCell
+
+@end
