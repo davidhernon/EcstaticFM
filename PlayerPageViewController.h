753cf4cd8bc0a88fad73fd19c0eb6d6e2d41ab41
davidhernon <david.hernon@mail.mcgill.ca>
2015-05-09 20:51:43 -0400

    Refactored PageViewController

---
+++ PlayerPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayerPageViewController.h
+//  EcstaticFM
+//
+//  Created by David Hernon on 2015-05-09.
+//  Copyright (c) 2015 David Hernon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerPageViewController : UIPageViewController
+
+@end
