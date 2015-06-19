059177ed278815b5a4e14832b59bc635f5951148
davidhernon <david.hernon@mail.mcgill.ca>
2015-06-19 17:14:41 -0400

    Added a workaround for when there is no location data from an iOS simulator

--- UIAroundMeView.m
+++ UIAroundMeView.m
@@ -44,11 +44,7 @@
     }
     if((self = [super initWithFrame:aRect]))
     {
-       /* while(isnan(_coord.longitude))
-        {
-            [NSThread sleepForTimeInterval:0.1f];
-        }
-*/
+
 		NSString *className = NSStringFromClass([self class]);
         self.view = [[[NSBundle mainBundle] loadNibNamed:className owner:self options:nil] firstObject];
         self.other_listeners.text = [NSString stringWithFormat:@"%@ and %lu other(s)", [room_info objectForKey:@"host_username"], [users count] - 1];
@@ -59,6 +55,11 @@
         
         NSNumber *host_lat = [host_location objectForKey:@"latitude"];
         NSNumber *host_lon = [host_location objectForKey:@"longitude"];
+        
+        if(host_lat == [NSNull null]){
+            host_lat = [NSNumber numberWithDouble:45.5017];
+            host_lon = [NSNumber numberWithDouble:-73.5673];
+        }
         
         _coord.latitude = [host_lat doubleValue];
         _coord.longitude = [host_lon doubleValue];
