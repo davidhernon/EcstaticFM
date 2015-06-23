3455d5d512fa8f0b07a0f156ce187f8f3919d66e
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-06-22 21:41:51 -0400

    chat bubble label

---
+++ ChatBubbleLabel.h
@@ -0,0 +1,13 @@
+//
+//  ChatBubbleLabel.h
+//  EcstaticFM
+//
+//  Created by Martin Weiss 1 on 2015-06-22.
+//  Copyright (c) 2015 David Hernon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatBubbleLabel : UILabel
+
+@end
